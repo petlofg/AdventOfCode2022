2772c4172e9ef66690060a69ffe5f727617983b7
petlofg <lofgren.petter@hotmail.com>
2022-12-06 22:19:57 +0100

    6

---
+++ 6/Program.cs
@@ -0,0 +1,22 @@
+﻿string input = File.ReadAllText(@".\input.txt");
+
+// Part 1
+PrintPosition(input, 4);
+
+// Part 2
+PrintPosition(input, 14);
+
+static void PrintPosition(string input, int windowLength)
+{
+	for (int i = 0; i < input.Length - windowLength; i++)
+	{
+		string window = input.Substring(i, windowLength);
+		HashSet<char> distinct = new(window);
+
+		if (distinct.Count == windowLength)
+		{
+			Console.WriteLine(i + windowLength);
+			break;
+		}
+	}
+}
